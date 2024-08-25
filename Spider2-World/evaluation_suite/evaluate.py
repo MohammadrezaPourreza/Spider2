df4fcb200d4e609e40cde7845ba3aad30614b9d8
lfy79001 <843265183@qq.com>
2024-08-25 16:35:27 +0800

    update

--- Spider2-World/evaluation_suite/evaluate.py
+++ Spider2-World/evaluation_suite/evaluate.py
@@ -85,8 +85,13 @@ def run_evaluation(result_dir, gold_dir):
         output_list.append(output_dict)
         
                 
-    print(output_list)
-    print(sum([entry['score'] for entry in output_list])/len(output_list), sum([entry['score'] for entry in output_list]), len(output_list))
+    score = 0
+    for item in output_list:
+        if item['score'] == 1:
+            score += 1
+            print(item['instance_id'])
+    print(score / len(output_list), score, len(output_list))
+    
 
 
     
